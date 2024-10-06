1bd3f9dc9db1c40355ad53a9e70751aedba3b008
Hajden <138399356+Hajdenko@users.noreply.github.com>
2024-10-06 20:19:44 +0200

    Optimized main.lua

--- main.lua
+++ main.lua
@@ -1,22 +1,39 @@
+local vehicleHash = -1553120962
+local fall = false
+
 Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(5)
-            if IsPedFalling(PlayerPedId()) then
-                SetEntityInvincible(PlayerPedId(), true)
-                SetPlayerInvincible(PlayerId(), true)
-                ClearPedBloodDamage(PlayerPedId())
-                ResetPedVisibleDamage(PlayerPedId())
-                ClearPedLastWeaponDamage(PlayerPedId())
-                SetEntityProofs(PlayerPedId(), true, true, true, true, true, true, true, true)
-                SetEntityOnlyDamagedByPlayer(PlayerPedId(), false)
-                SetEntityCanBeDamaged(PlayerPedId(), false)
-            else
-                SetEntityInvincible(PlayerPedId(), false)
-                SetPlayerInvincible(PlayerId(), false)
-                ClearPedLastWeaponDamage(PlayerPedId())
-                SetEntityProofs(PlayerPedId(), false, false, false, false, false, false, false, false)
-                SetEntityOnlyDamagedByPlayer(PlayerPedId(), false)
-                SetEntityCanBeDamaged(PlayerPedId(), true)
-            end 
-	end
+    while true do
+        local playerPed = PlayerPedId()
+        SetWeaponDamageModifier(vehicleHash, 0.0)
+        SetPedCanRagdoll(playerPed, false)
+        SetPedCanRagdollFromPlayerImpact(playerPed, false)
+        -- SetPedRagdollOnCollision(playerPed, false)
+        SetPlayerFallDistance(PlayerId(), 1000000.0)
+        SetPlayerCanDoDriveBy(PlayerId(), false)
+        local vehicle = GetVehiclePedIsIn(playerPed, false)
+        if vehicle ~= 0 then
+            SetVehicleCheatPowerIncrease(vehicle, 5.0)
+            SetVehicleGravityAmount(vehicle, 11.0)
+        end
+        Wait(1000)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        local playerPed = PlayerPedId()
+        local coords = GetEntityCoords(playerPed)
+        local _, ground = GetGroundZFor_3dCoord(coords.x, coords.y, coords.z, false)
+        if not fall and IsPedFalling(playerPed) and coords.z - ground < 2.0 then
+            ClearPedTasks(playerPed)
+            fall = true
+        elseif not IsPedFalling(playerPed) then
+            fall = false
+        end
+        if IsPedFalling(playerPed) then
+            Wait(0)
+        else
+            Wait(500)
+        end
+    end
 end)
